6a7db5d2f54bb6036ba4109339c9368b2e1311f5
jeongeun <jeuun12@gmail.com>
2019-11-01 17:12:24 +0900

    Signed-off-by: jeongeun <jeuun12@gmail.com>

--- 2019_10_31.sql
+++ 2019_10_31.sql
@@ -88,12 +88,12 @@ SELECT *
 FROM emp
 WHERE ename LIKE 'S____';
 
--- where4
+-- 실습 where4
 SELECT mem_id, mem_name
 FROM member
 WHERE mem_name LIKE '신__';
 
--- where5
+-- 실습 where5
 SELECT mem_id, mem_name
 FROM member
 WHERE mem_name LIKE '%이%'; -- 문자열안에 이로 시작하는 데이터 
@@ -102,5 +102,90 @@ WHERE mem_name LIKE '이%'; -- mem_name에 첫 단어가 이로 시작하는 데이터
 -- NULL 비교
 -- colc IS NULL
 -- EMP 테이블에서 MGR 정보가 없는 사람(NULL) 조회
+SELECT *
+FROM emp
+WHERE mgr IS NULL;
+-- WHERE MGR != null;   -- null 비교가 실패한다
+
+-- 소속 부서가 10번이 아닌 직원들
+SELECT *
+FROM emp
+WHERE deptno != '10';
+
+-- 실습 where6
+SELECT *
+FROM emp
+WHERE COMM IS NOT NULL;
+
+-- AND/OR
+-- 관리자(mgr) 사번이 7698 이고 급여가 1000이상 인 사람
+SELECT *
+FROM emp
+WHERE mgr = 7698
+AND sal >= 1000;
+
+-- emp 테이블에서 관리자(mgr) 사번이 7698 이거나 급여(sal)가 1000 이상인 직원 조회
+SELECT *
+FROM emp
+WHERE mgr = 7698
+OR sal >= 1000;
+
+-- emp 테이블에서 관리자(mgr) 사번이 7698이 아니고, 7839가 아닌 직원들 조회
+SELECT *
+FROM emp
+WHERE mgr NOT IN (7698,7639);   -- NOT IN --> AND
+
+-- 위의 쿼리를 AND/OR 연산자로 변환
+SELECT *
+FROM emp
+WHERE mgr != 7698
+AND mgr != 7639;
+
+-- IN, NOT IN 연산자의 NULL 처리
+-- emp 테이블에서 관리자(emp) 사번이 7698, 7939 또는 null이 아닌 직원들 조회
+SELECT *
+FROM emp
+WHERE mgr NOT IN (7698,7639,NULL); -- IN 연산자에서 결과값에 NULL이 있을 경우 의도하지 않는 동작을 한다.
+
+SELECT *
+FROM emp
+WHERE mgr NOT IN (7698,7839)
+AND mgr IS NOT NULL;
+
+-- 실습 where7
+SELECT *
+FROM emp
+WHERE job = 'SALESMAN'     -- 문자열 중요
+AND hiredate >= TO_DATE('1981/06/01','YYYY/MM/DD');
+
+-- 실습 where8 
+SELECT *
+FROM emp
+WHERE DEPTNO != 10
+AND hiredate >= TO_DATE('1981/06/01','YYYY/MM/DD');
+
+-- 실습 where9
+SELECT *
+FROM emp
+WHERE DEPTNO NOT IN (10)
+AND hiredate >= TO_DATE('1981/06/01','YYYY/MM/DD');
+
+-- 실습 where10
+SELECT *
+FROM emp
+WHERE DEPTNO NOT IN 10
+AND hiredate >= TO_DATE('1981/06/01','YYYY/MM/DD');
+
+-- 실습 where11
+SELECT *
+FROM emp
+WHERE job = 'SALESMAN'
+OR hiredate >= TO_DATE('1981/06/01','YYYY/MM/DD');
+
+-- where12
+SELECT *
+FROM emp
+WHERE job = 'SALESMAN'
+OR EMPNO LIKE '78%';
 
 
